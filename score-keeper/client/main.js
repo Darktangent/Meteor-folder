9ddf0bc923486c83b32b210319300e298bb8764b
Ron Ganguly <c1ph3r9@gmail.com>
2018-12-03 10:44:31 -0600

    initial proj files

---
+++ score-keeper/client/main.js
@@ -0,0 +1,8 @@
+// import './../imports/utils'
+
+import someDefault,{greetUser, name} from "./../imports/utils"
+
+
+console.log(greetUser())
+console.log(someDefault);
+console.log('Log from /client/main.js')
---
+++ score-keeper/server/main.js
@@ -0,0 +1,8 @@
+// import "./../imports/utils"
+import {greetUser} from "./../imports/utils"
+import{add} from "./../imports/math"
+console.log(greetUser())
+console.log(add(10,22))
+
+
+console.log('Log from server/main.js')
